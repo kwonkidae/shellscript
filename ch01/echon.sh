7f206941a44edb1345ffc65f10f17204ca6a78f3
kwon <kkdosk@naver.com>
2019-04-10 20:16:11 +0900

    a

---
+++ ch01/echon.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+echon()
+{
+  printf "%s\n" "$*"
+}
+
+echon "$@"
---
+++ ch01/opts.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+
+# while getopts ":ab:c" opt; do
+#   case $opt in
+#     a) echo "-a" ;;
+#     b) 
+#        echo "$OPTARG is the options argument" ;;
+#   esac
+# done
+
+# echo "$OPTIND"
