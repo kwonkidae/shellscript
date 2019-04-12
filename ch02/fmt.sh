eceb9805758751000be4c94c710ed1d8239daec1
kwon <kkdosk@naver.com>
2019-04-12 11:38:55 +0900

    add

---
+++ ch02/fmt.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+while getopts "hw:" opt; do
+  case $opt in
+    h ) hyph=1            ;;
+    w ) width="$OPTARG"   ;;  
+  esac
+done
+shift $(($OPTIND - 1))
+
+nroff << EOF
+.ll ${width:-72}
+.na
+.hy ${hyph:-0}
+.pl 1
+$(cat "$@")
+EOF
+
+exit 0
